a19351f5a673f2b2906886b3c1dfdc253f232d17
tommyzhng <tommyz2005@gmail.com>
2022-10-25 18:03:20 -0400

    add threading and box drawing

--- Programming&Drone Development/personTrackingDrone/bodyTracking/tensorflowMethod/tracker.py
+++ Programming&Drone Development/personTrackingDrone/bodyTracking/tensorflowMethod/tracker.py
@@ -6,13 +6,28 @@ import cv2 as cv
 import numpy as np
 import os
 
+class GetVideo(): #get video from another thread to reduce latency
+    def __init__(self) -> None:
+        self.stream = cv.VideoCapture(0)
+        (self.success, self.frame) = self.stream.read() #read the first frame
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.getframes, args=()).start() #start a thread to read frames
+        return self
+
+    def getframes(self):
+        while not self.stopped:
+            if not self.success: ##if cant grab frame, then stop the thread
+                self.stop()
+            else:
+                (self.success, self.frame) = self.stream.read()
+    def stop(self):
+        self.stopped = True
+
 
 class Tracker():
     def __init__(self) -> None:
-        ###### start video capture ######
-        self.capture = cv.VideoCapture(0)
-        #################################
-        
         #if this is running on raspi, use import from rflite runtime
         if importlib.util.find_spec('tflite_runtime'):
             from tflite_runtime.interpreter import Interpreter
@@ -38,16 +53,14 @@ class Tracker():
         
         self.frame_rate_calc = 1
         self.freq = cv.getTickFrequency()  
- 
-    def read(self):
-        success, frame = self.capture.read()
-        return frame
 
     def process(self, frame):
+        frame = cv.resize(frame, (640,480))
         frame = cv.flip(frame, 1)
+        frame = cv.rotate(frame, cv.ROTATE_90_CLOCKWISE)
         t1 = cv.getTickCount()
         
-        framergb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        framergb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)     ##create another frame to process on
         frame1 = cv.resize(framergb, (self.width, self.height))
         
         data = np.expand_dims(frame1, axis=0)
@@ -60,18 +73,45 @@ class Tracker():
         classes = self.interpreter.get_tensor(self.output_det[1]['index'])[0] 
         #Confidence level
         confidence = self.interpreter.get_tensor(self.output_det[2]['index'])[0] 
-        
-        for i in range(len(confidence)):
-            if (self.labels[int(classes[i])] == "person"):
-                if (confidence[i] > 0.6 and confidence[i] < 1.0): #if confidence is high, run the bounding boxes
-                    pass      
+
+        center = (0,0)
+
+        if (self.labels[int(classes[0])] == "person"):
+            if (confidence[0] > 0.4 and confidence[0] < 1.0): #if confidence is high, run the bounding boxes
+                    minx, maxx = int(max(1,(bbox[0][1] * 480))), int(min(640,(bbox[0][3] * 480)))
+                    miny, maxy = int(max(1,(bbox[0][0] * 640))), int(min(640,(bbox[0][2] * 640)))
+                    center = round((maxx + minx)/2, 3), round((maxy + miny)/2, 3)
+
+                    cv.circle(frame, (int(center[0]), int(center[1])), 15, (0, 0, 255), -1)
+                    cv.rectangle(frame, (minx,miny), (maxx, maxy), (0,255,0), 2)
+
+        differences = self.distanceFromCenter(frame, center)
 
         cv.putText(frame,f'FPS: {round(self.frame_rate_calc)}',(30,50),cv.FONT_HERSHEY_SIMPLEX,1,(0,255,0),1,cv.LINE_AA)
         t2 = cv.getTickCount()
         time1 = (t2-t1)/self.freq
         self.frame_rate_calc= 1/time1
 
-        return frame
+        
+        return frame, differences
+
+    def distanceFromCenter(self, frame, center):
+            #Center
+            if center != (0, 0):
+                #Draw circle at center of screen
+                cv.circle(frame, (int(frame.shape[1]/2), int(frame.shape[0]/2)), 15, (255, 0, 0), -1)
+
+                #Draw a line connecting the two points
+                cv.line(frame, (240, 320), (int(center[0]), int(center[1])), (255, 255, 0), 10)
+
+                #Find X Y differences           #X                                      #Y
+                differences = ((0.5 - center[0]/480), (center[1]/640 - 0.5))
+                cv.putText(frame, f"X Delta = {round(differences[0] * 100, 3)}%", (340, 240), cv.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+                cv.putText(frame, f"Y Delta = {round(differences[1] * 100, 3)}%", (340, 270), cv.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+            else:
+                differences = (0, 0)
+
+            return differences
     
 
         
