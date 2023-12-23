7274c712d16d04296228f5a3197dad4703bce4a5
Tommy Zhang <tommyz2005@gmail.com>
2023-12-22 19:57:57 -0500

    Remove unnecessary files and update drone instructions

--- Projects/hoverslamDrone/drone.py
+++ Projects/hoverslamDrone/drone.py
@@ -32,12 +32,13 @@ class Drone:
         d = self.vehicle.location.global_relative_frame.alt - 0.5
 
         a = ((v2**2)-(v1**2)) / (2*(d))
-        print(f"v: {v1}, d: {d}, a: {a}")
-        time.sleep(0.1)
+        #a = (v2 - v1) / 2
+        print(f"current v: {v1}, d to ground: {d}, commanded accel: {a}")
+        time.sleep(0.2)
         return a
 
     def send_acceleration(self, freefall = False):
-        a = self.calculate_target_acceleration() if not freefall else 9.8
+        a = self.calculate_target_acceleration() if not freefall else 10
         msg = self.vehicle.message_factory.set_position_target_local_ned_encode(
             0,
             0, 0,
@@ -48,3 +49,24 @@ class Drone:
             0, 0, a,
             0, 0)
         self.vehicle.send_mavlink(msg) 
+
+
+    def yaw_instruction(self):
+        if self.vehicle.location.global_relative_frame.alt < 10:
+            a = self.calculate_acceleration()
+            v = self.vehicle.velocity[2]
+        else:
+            v = 3.5
+            a = 0
+        msg = self.vehicle.message_factory.set_position_target_local_ned_encode(
+            0,
+            0, 0,
+            mavutil.mavlink.MAV_FRAME_BODY_NED,
+            0b010000000111,
+            0, 0, 0, #position
+            5, 0, v, #velocity
+            0, 0, a, #acceleration
+            0, 1.3)    #yaw
+        self.vehicle.send_mavlink(msg)
+        self.request_data()
+        time.sleep(0.2)
--- Projects/hoverslamDrone/main.py
+++ Projects/hoverslamDrone/main.py
@@ -4,10 +4,10 @@ import time
 drone = Drone()
 
 drone.arm_and_takeoff(50)
+
 while drone.vehicle.armed == True:
-    (drone.send_acceleration() if drone.vehicle.location.global_relative_frame.alt <= 45 
-                                            else drone.send_acceleration(freefall=True))
-    if drone.vehicle.location.global_relative_frame.alt <= 1:
+    drone.send_acceleration() if drone.vehicle.location.global_relative_frame.alt <= 45 else drone.send_acceleration(freefall=True)
+    if drone.vehicle.location.global_relative_frame.alt <= 2:
         break
     
 drone.vehicle.mode = VehicleMode("LAND")
