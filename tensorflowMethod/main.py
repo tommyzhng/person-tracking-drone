ab42e6e3908e5568c3df4f9f07c01e3e07a56df4
Tommy Zhang <tommyz2005@gmail.com>
2023-12-28 17:51:44 -0500

    update with PID control

--- tensorflowMethod/main.py
+++ tensorflowMethod/main.py
@@ -4,24 +4,30 @@ from drone import DroneFunctions, VehicleMode
 import time
 import keyboard
 
-#OpenCV Init
+#OpenCV Inits
 drone = DroneFunctions(testing=True)
 capture = GetVideo().start()
 tracker = Tracker() 
 
+start_tracking = False
+
+print("done setup")
 
 #Main Loop
 while True:
-    frame = capture.frame
-    frame, differences, area = tracker.process(frame=frame)
-    drone.move(differences, area)
-    print(f"{round(differences*100, 3)} %")
-    print(f"Area: {area}")
-    cv.imshow("tracker", frame)
+    if start_tracking:
+        frame = capture.frame
+        frame, differences, area = tracker.process(frame=frame)
+        xDiff, velocity = drone.calculate_movement(differences, area)
+        #print(f"{round(differences*100, 3)} %")
+        print(f"xDiff: {xDiff}, velocity: {velocity}")
+        cv.imshow("tracker", frame)
+    if keyboard.is_pressed('s'):
+        start_tracking = True
 
     #if t is pressed, takeoff
     if keyboard.is_pressed('t'):
-        drone.arm_and_takeoff(2)
+        drone.arm_and_takeoff(3)
     
     #if l is pressed, land
     if keyboard.is_pressed('l'):
