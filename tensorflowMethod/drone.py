c2a9d8d0a5097781800556ac012b4d72ae8a2d75
Tommy Zhang <tommyz2005@gmail.com>
2023-12-28 20:39:24 -0500

    Update connection string for testing

--- tensorflowMethod/drone.py
+++ tensorflowMethod/drone.py
@@ -7,7 +7,8 @@ from simple_pid import PID
 class DroneFunctions:
     def __init__(self, testing = False):
         if testing == True:
-            connectionString = socket.gethostbyname_ex(socket.gethostname())[-1][1] + ":14550"
+            #connectionString = socket.gethostbyname_ex(socket.gethostname())[-1][1] + ":14550"
+            connectionString = "192.168.124.246:14550"
         else:
             connectionString = "/dev/ttyAMA0"
         print(connectionString)
