a272726b33a5cbf2c18fd6e46de91e0a23a87626
Tommy Zhang <tommyz2005@gmail.com>
2023-12-28 20:38:10 -0500

    Add keyboard listener for starting tracking

--- tensorflowMethod/main.py
+++ tensorflowMethod/main.py
@@ -1,42 +1,32 @@
 import cv2 as cv
 from tracker import GetVideo, Tracker
 from drone import DroneFunctions, VehicleMode
+from keyboard import KeyboardListener
 import time
-import keyboard
 
 #OpenCV Inits
 drone = DroneFunctions(testing=True)
 capture = GetVideo().start()
-tracker = Tracker() 
+tracker = Tracker()
+keyboard = KeyboardListener(drone)
+keyboard.start()
 
-start_tracking = False
-
-print("done setup")
+print("Setup complete. Press 's' to start tracking.")
 
 #Main Loop
 while True:
-    if start_tracking:
+    if keyboard.start_tracking:
         frame = capture.frame
         frame, differences, area = tracker.process(frame=frame)
         xDiff, velocity = drone.calculate_movement(differences, area)
         #print(f"{round(differences*100, 3)} %")
         print(f"xDiff: {xDiff}, velocity: {velocity}")
         cv.imshow("tracker", frame)
-    if keyboard.is_pressed('s'):
-        start_tracking = True
 
-    #if t is pressed, takeoff
-    if keyboard.is_pressed('t'):
-        drone.arm_and_takeoff(3)
-    
-    #if l is pressed, land
-    if keyboard.is_pressed('l'):
-        drone.vehicle.mode = VehicleMode("LAND")
-
-    #if q is pressed, quit
-    if cv.waitKey(1) == ord('q'):
-        drone.vehicle.mode = VehicleMode("LAND")
-        capture.stop()
-        break
+        #if q is pressed, quit
+        if cv.waitKey(1) == ord('q'):
+            drone.vehicle.mode = VehicleMode("LAND")
+            capture.stop()
+            break
 
     time.sleep(0.01)
